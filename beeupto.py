4889f893ffaa42126d40b0b40998045bb827be1c
Sabarish JS <71866723+SabarishJS@users.noreply.github.com>
2024-07-01 13:13:24 +0530

    Update beeupto.py

--- beeupto.py
+++ beeupto.py
@@ -94,7 +94,7 @@ st.title("Cricket Ball Trajectory Plotter")
 # Upload CSV file
 
 def main():
-    data = pd.read_csv(NewData)
+    data = pd.read_csv('NewData.csv')
 
     # Date filter
     data['Date'] = pd.to_datetime(data['date'], dayfirst=True)
